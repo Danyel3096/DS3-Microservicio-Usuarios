e013cb14f2e98aed0fefea82c82816f28f3ae624
JuanLopezAranzazu <juanlopezaranzazu@gmail.com>
2025-03-12 21:26:35 -0500

    Estructura del microservicio

---
+++ src/main/java/com/ds3/team8/users_service/UsersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ds3.team8.users_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds3/team8/users_service/UsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds3.team8.users_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
