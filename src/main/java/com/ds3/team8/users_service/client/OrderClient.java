aecec77fd9f61517a1bf162fbdc98e6f58177583
Diego Tolosa <tolosad83@gmail.com>
2025-06-23 22:26:12 -0500

    Feat: Agregar cliente Feign para verificar si un usuario tiene órdenes

---
+++ src/main/java/com/ds3/team8/users_service/client/OrderClient.java
@@ -0,0 +1,15 @@
+package com.ds3.team8.users_service.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.ds3.team8.users_service.config.FeignClientInterceptor;
+
+@FeignClient(name = "orders-service", configuration = FeignClientInterceptor.class)
+public class OrderClient {
+    
+    @GetMapping("/api/v1/orders/user/{userId}/exists")
+    Boolean userHasOrders(@PathVariable("userId") Long userId);
+
+}
