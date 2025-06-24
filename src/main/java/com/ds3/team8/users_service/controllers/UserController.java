9a2ec7ec72e00930dc4ddf58754d8e2360474ee4
Diego Tolosa <tolosad83@gmail.com>
2025-06-23 22:35:02 -0500

    Feat: Agregar validación de roles y documentación en UserController

--- src/main/java/com/ds3/team8/users_service/controllers/UserController.java
+++ src/main/java/com/ds3/team8/users_service/controllers/UserController.java
@@ -2,7 +2,13 @@ package com.ds3.team8.users_service.controllers;
 
 import com.ds3.team8.users_service.dtos.UserRequest;
 import com.ds3.team8.users_service.dtos.UserResponse;
+import com.ds3.team8.users_service.enums.Role;
 import com.ds3.team8.users_service.services.IUserService;
+import com.ds3.team8.users_service.utils.SecurityUtil;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,59 +20,81 @@ import java.util.List;
 
 @RestController // Indica que esta clase es un controlador REST
 @RequestMapping("/api/v1/users") // Indica la URL base para acceder a los servicios de esta clase
+@Tag(name = "Usuarios", description = "Endpoints para usuarios")
 public class UserController {
 
-    private IUserService userService;
+    private final IUserService userService;
 
     public UserController(IUserService userService){
         this.userService = userService;
     }
 
     // Obtener todos los usuarios
+    @Operation(summary = "Obtener todos los usuarios", description = "Obtener todos los usuarios del sistema.", security = { @SecurityRequirement(name = "Bearer Authentication") })
     @GetMapping
-    public ResponseEntity<List<UserResponse>> getAllUsers() {
+    public ResponseEntity<List<UserResponse>> getAllUsers(
+        @RequestHeader("X-Authenticated-User-Role") String roleHeader
+    ) {
+        SecurityUtil.validateRole(roleHeader, Role.ADMIN);
         return ResponseEntity.ok(userService.findAll());
     }
 
     // Crear un usuario
+    @Operation(summary = "Guardar un usuario", description = "Guardar un usuario en el sistema.", security = { @SecurityRequirement(name = "Bearer Authentication") })
     @PostMapping
-    public ResponseEntity<UserResponse> saveUser(@Valid @RequestBody UserRequest userRequest) {
+    public ResponseEntity<UserResponse> saveUser(
+        @Valid @RequestBody UserRequest userRequest,
+        @RequestHeader("X-Authenticated-User-Role") String roleHeader
+    ) {
+        SecurityUtil.validateRole(roleHeader, Role.ADMIN);
         UserResponse savedUser = userService.save(userRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
     }
 
     // Actualizar un usuario
+    @Operation(summary = "Actualizar un usuario", description = "Actualizar un usuario en el sistema.", security = { @SecurityRequirement(name = "Bearer Authentication") })
     @PutMapping("/{id}")
     public ResponseEntity<UserResponse> updateUser(
             @PathVariable Long id,
-            @Valid @RequestBody UserRequest userRequest) {
+            @Valid @RequestBody UserRequest userRequest,
+            @RequestHeader("X-Authenticated-User-Role") String roleHeader
+    ) {
+        SecurityUtil.validateRole(roleHeader, Role.ADMIN);
         UserResponse updatedUser = userService.update(id, userRequest);
         return ResponseEntity.ok(updatedUser);
     }
 
     // Buscar usuarios con paginación
-    // Cambia la URL a algo como /api/v1/users/pageable?page=0&size=10&sort=firstName,asc
-    // Para el sort= se puede usar cualquier atributo de la entidad User
-    // Por ejemplo para ordenar por apellido desde la Z a la A sería sort=lastName,desc
-    // Para el caso de roles se puede usar sort=role.name,asc (sort=entity.attribute,asc o desc)
+    // Ejemplo URL /api/v1/users/pageable?page=0&size=8
+    @Operation(summary = "Obtener los usuarios con paginación", description = "Obtener los usuarios con paginación del sistema.", security = { @SecurityRequirement(name = "Bearer Authentication") })
     @GetMapping("/pageable")
-    public Page<UserResponse> findAllPageable(Pageable pageable) {
+    public Page<UserResponse> findAllPageable(
+        Pageable pageable,
+        @RequestHeader("X-Authenticated-User-Role") String roleHeader
+    ) {
+        SecurityUtil.validateRole(roleHeader, Role.ADMIN);
         return userService.findAllPageable(pageable);
     }
 
-
     // Obtener un Usuario por ID
+    @Operation(summary = "Obtener un usuario", description = "Obtener un usuario por su id del sistema.", security = { @SecurityRequirement(name = "Bearer Authentication") })
     @GetMapping("/{id}")
-    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+    public ResponseEntity<UserResponse> getUserById(
+        @PathVariable Long id
+    ) {
         return ResponseEntity.ok(userService.findById(id));
     }
 
     // Eliminación lógica de un usuario
+    @Operation(summary = "Eliminar un usuario", description = "Eliminar un usuario por su id en el sistema.", security = { @SecurityRequirement(name = "Bearer Authentication") })
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteUser(
+        @PathVariable Long id,
+        @RequestHeader("X-Authenticated-User-Role") String roleHeader
+    ) {
+        SecurityUtil.validateRole(roleHeader, Role.ADMIN);
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
-
 
 }
