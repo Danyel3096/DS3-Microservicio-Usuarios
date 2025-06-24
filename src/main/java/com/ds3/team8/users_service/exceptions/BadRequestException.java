c9a8379e9176e8d8d8491f504b943a62bc801e76
Diego Tolosa <tolosad83@gmail.com>
2025-06-23 22:50:24 -0500

    Feat: Agregar clase BadRequestException para manejo de errores de solicitud incorrecta

---
+++ src/main/java/com/ds3/team8/users_service/exceptions/BadRequestException.java
@@ -0,0 +1,15 @@
+package com.ds3.team8.users_service.exceptions;
+
+public class BadRequestException {
+    public BadRequestException(String message){
+        super(message);
+    }
+
+    public BadRequestException(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    public BadRequestException(Throwable cause){
+        super(cause);
+    }
+}
