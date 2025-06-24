70d9aeacea9d3209b3365084ea521b8858e83ae5
Diego Tolosa <tolosad83@gmail.com>
2025-06-23 22:51:26 -0500

    Feat: Agregar clase NotFoundException para manejo de errores de recurso no encontrado

---
+++ src/main/java/com/ds3/team8/users_service/exceptions/NotFoundException.java
@@ -0,0 +1,15 @@
+package com.ds3.team8.users_service.exceptions;
+
+public class NotFoundException {
+    public NotFoundException(String message){
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    public NotFoundException(Throwable cause){
+        super(cause);
+    }
+}
