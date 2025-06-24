5865902ab26e08c3b56440db74cd5e3c06c497b6
Diego Tolosa <tolosad83@gmail.com>
2025-06-23 22:49:25 -0500

    Feat: Eliminar excepciones de rol y usuario, y agregar clase AccessDeniedException

---
+++ src/main/java/com/ds3/team8/users_service/exceptions/AccessDeniedException.java
@@ -0,0 +1,15 @@
+package com.ds3.team8.users_service.exceptions;
+
+public class AccessDeniedException {
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+
+    public AccessDeniedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AccessDeniedException(Throwable cause) {
+        super(cause);
+    }
+}
