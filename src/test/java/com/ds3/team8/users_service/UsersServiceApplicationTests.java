c8b8b77b6837e9daab4f30a536cff16d19c8a091
Diego Tolosa <tolosad83@gmail.com>
2025-06-25 21:42:38 -0500

    Feat: Rehabilitar la anotación @Disabled en las pruebas de contexto de UsersServiceApplication

--- src/test/java/com/ds3/team8/users_service/UsersServiceApplicationTests.java
+++ src/test/java/com/ds3/team8/users_service/UsersServiceApplicationTests.java
@@ -1,8 +1,10 @@
 package com.ds3.team8.users_service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("Disabled in CI pipeline")
 @SpringBootTest
 class UsersServiceApplicationTests {
 
